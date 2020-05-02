d445d8e8de88456f169e249ff4e23712d99f277a
nnhattruong30 <nnhattruong30@gmail.com>
2020-05-02 11:56:34 +0700

    Add *

---
+++ grad_desc_tf2.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+
+theta = tf.Variable(-10.0)
+
+@tf.function
+def Loss(thete):
+    return theta**2 - 4*theta + 10
+
+alpha = 0.01
+eps = 0.0001
+
+opt = tf.optimizers.SGD(lr = alpha)
+
+while True: 
+    with tf.GradientTape() as tape:
+        loss = Loss(theta)
+        print(loss)
+    
+    grads = tape.gradient(loss, [theta])
+    opt.apply_gradients(zip(grads, [theta]))
+    if abs(grads[0].numpy()) < eps: break
+
+print(theta)
